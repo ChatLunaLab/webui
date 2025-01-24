80ea724ad442640973a2c6ca7a811cbe4b862dea
dingyi <dingyi222666@foxmail.com>
2025-01-25 00:00:18 +0800

    feat(workspace-nav): add WorkspaceNav component with navigation links

---
+++ src/components/layout/workspace/WorkspaceNav.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: string
+}>()
+</script>
+
+<template>
+  <nav
+    :class="cn('flex items-center space-x-4 lg:space-x-6', props.class ?? '')"
+  >
+    <a
+      href="/examples/dashboard"
+      class="text-sm font-medium transition-colors hover:text-primary"
+    >
+      助手
+    </a>
+   
+    <a
+      href="/examples/dashboard"
+      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+    >
+      知识库
+    </a>
+    <a
+      href="/examples/dashboard"
+      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+    >
+      数据
+    </a>
+  </nav>
+</template>
--- src/pages/Workspace.vue
+++ src/pages/Workspace.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { TopBar, TopBarIcon } from '@/components/layout/topbar'
+import WorkspaceNav from '@/components/layout/workspace/WorkspaceNav.vue';
 
 </script>
 
@@ -7,7 +8,11 @@ import { TopBar, TopBarIcon } from '@/components/layout/topbar'
   <!-- body -->
   <div class="flex-1 h-full w-full">
     <div class="flex flex-col h-[100vh] w-full">
-      <TopBar class="justify-between"></TopBar>
+      <TopBar class="justify-between">
+        <template v-slot:navigation>
+          <WorkspaceNav class="ml-6" />
+        </template>
+      </TopBar>
     </div>
   </div>
 </template>
